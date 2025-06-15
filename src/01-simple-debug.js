0199e7d0bdf3927b78836574029351c734e62044
Coder Hillary <coderhillary@192.168.1.215>
2025-06-15 21:12:48 +0300

    complete lab

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,268 @@
-console.log('Simple debugging example running.')
-debugger
+//console.log('Simple debugging example running.')
+//debugger
 
-let x = 99
-debugger
-console.log(x)
+//let x = 99
+//debugger
+//console.log(x)
+
+function gameObject() {
+  return {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
+        },
+        "Reggie Evans": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15,
+        },
+        "Mason Plumlee": {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5,
+        },
+        "Jason Terry": {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1,
+        },
+      },
+    },
+    away: {
+      teamName: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien": {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slamDunks: 2,
+        },
+        "Bismak Biyombo": {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slamDunks: 10,
+        },
+        "DeSagna Diop": {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slamDunks: 5,
+        },
+        "Ben Gordon": {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slamDunks: 0,
+        },
+        "Brendan Haywood": {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slamDunks: 12,
+        },
+      },
+    },
+  };
+}
+
+function numPointsScored(playerName) {
+  const game = gameObject();
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    if (players[playerName]) {
+      return players[playerName].points;
+    }
+  }
+  return null;
+}
+
+function shoeSize(playerName) {
+  const game = gameObject();
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    if (players[playerName]) {
+      return players[playerName].shoe;
+    }
+  }
+  return null;
+}
+
+function teamColors(teamName) {
+  const game = gameObject();
+  for (let teamKey in game) {
+    if (game[teamKey].teamName === teamName) {
+      return game[teamKey].colors;
+    }
+  }
+  return null;
+}
+
+function teamNames() {
+  const game = gameObject();
+  return [game.home.teamName, game.away.teamName];
+}
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+  for (let teamKey in game) {
+    if (game[teamKey].teamName === teamName) {
+      return Object.values(game[teamKey].players).map(player => player.number);
+    }
+  }
+  return [];
+}
+
+function playerStats(playerName) {
+  const game = gameObject();
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    if (players[playerName]) {
+      return players[playerName];
+    }
+  }
+  return null;
+}
+
+function bigShoeRebounds() {
+  const game = gameObject();
+  let maxShoeSize = 0;
+  let rebounds = 0;
+
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    for (let player in players) {
+      if (players[player].shoe > maxShoeSize) {
+        maxShoeSize = players[player].shoe;
+        rebounds = players[player].rebounds;
+      }
+    }
+  }
+  return rebounds;
+}
+
+// Bonus functions
+
+function mostPointsScored() {
+  const game = gameObject();
+  let maxPoints = 0;
+  let topScorer = "";
+
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    for (let player in players) {
+      if (players[player].points > maxPoints) {
+        maxPoints = players[player].points;
+        topScorer = player;
+      }
+    }
+  }
+  return topScorer;
+}
+
+function winningTeam() {
+  const game = gameObject();
+  let homePoints = 0;
+  let awayPoints = 0;
+
+  for (let player in game.home.players) {
+    homePoints += game.home.players[player].points;
+  }
+  for (let player in game.away.players) {
+    awayPoints += game.away.players[player].points;
+  }
+
+  if (homePoints > awayPoints) return game.home.teamName;
+  else if (awayPoints > homePoints) return game.away.teamName;
+  else return "Tie";
+}
+
+function playerWithLongestName() {
+  const game = gameObject();
+  let longestName = "";
+
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    for (let player in players) {
+      if (player.length > longestName.length) {
+        longestName = player;
+      }
+    }
+  }
+  return longestName;
+}
+
+function doesLongNameStealATon() {
+  const game = gameObject();
+  const longestName = playerWithLongestName();
+  let maxSteals = 0;
+  let topStealer = "";
+
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+    for (let player in players) {
+      if (players[player].steals > maxSteals) {
+        maxSteals = players[player].steals;
+        topStealer = player;
+      }
+    }
+  }
+  return longestName === topStealer;
+}
+
